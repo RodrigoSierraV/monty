48d0dfff9530cdb23583321bc107cdd7cd098459
RodrigoSierraV <735@holbertonschool.com>
2019-05-16 21:40:55 +0000

    Adding swap opcode

--- swap_op.c
+++ swap_op.c
@@ -12,9 +12,23 @@ void swap_op(stack_t **stack, unsigned int line_number)
 
 	if (!*stack || !(*stack)->next)
 	{
-		dprintf(STDERR_FILENO, "can't swap, stack too short\n");
+		dprintf(STDERR_FILENO, "L%u: can't swap, stack too short\n",
+			line_number);
 		exit(EXIT_FAILURE);
 	}
-	(void)*stack;
-	(void)line_number;
+	if (!(*stack)->next->next)
+	{
+		*stack = (*stack)->next;
+		(*stack)->next = (*stack)->prev;
+		(*stack)->prev->next = NULL;
+		(*stack)->prev = NULL;
+		(*stack)->next->next = NULL;
+		return;
+	}
+	*stack = (*stack)->next;
+	(*stack)->prev->next = (*stack)->next;
+	(*stack)->prev->prev = *stack;
+	(*stack)->next->prev = (*stack)->prev;
+	(*stack)->next = (*stack)->prev;
+	(*stack)->prev = NULL;
 }
