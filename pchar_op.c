152e996bf65ee0c4d3aae2cc24f781915753544d
Madez17 <781@holbertonschool.com>
2019-05-17 04:46:00 +0000

    Fixing pchar function

--- pchar_op.c
+++ pchar_op.c
@@ -14,7 +14,7 @@ void pchar_op(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n >= 254)
+	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
