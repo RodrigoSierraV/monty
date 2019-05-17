12605aefb1236f7097485e8ce9e2d633894dd44d
GioRubioHolberton <759@holbertonschool.com>
2019-05-17 04:17:09 +0000

    conditions in pchar funtion

--- pchar_op.c
+++ pchar_op.c
@@ -9,6 +9,15 @@
 
 void pchar_op(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n < 0 || (*stack)->n >= 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	printf("%c\n", (*stack)->n);
 }
