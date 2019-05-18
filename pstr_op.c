4a375ddc65c04616b71af6fecce56328763fe487
RodrigoSierraV <47121619+RodrigoSierraV@users.noreply.github.com>
2019-05-18 10:31:03 -0500

    Fixing ascii codes

--- pstr_op.c
+++ pstr_op.c
@@ -12,7 +12,7 @@ void pstr_op(stack_t **stack, unsigned int line_number)
 	stack_t *aux = *stack;
 
 	(void)line_number;
-	while (aux->next && aux->n != 0 && aux->n => 0 && aux->n <= 127)
+	while (aux->next && aux->n != 0 && aux->n >= 0 && aux->n <= 127)
 	{
 		printf("%c", aux->n);
 		aux = aux->next;
